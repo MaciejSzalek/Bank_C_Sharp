ff7416890ad2ca4f5e78c0342f728f99ad986097
Maciek <maciejszalek84@gmail.com>
2019-08-23 12:04:57 +0200

    commit one

---
+++ Bank_C_Sharp/Printer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_C_Sharp
+{
+    class Printer
+    {
+        public void Print(SavingAccount account)
+        {
+            Console.WriteLine("Account owner: {0}", account.GetFullName());
+            Console.WriteLine("Account number: {0}", account.accountNumber);
+            Console.WriteLine("Balance: {0}", account.GetBalance());
+            Console.WriteLine("First name: {0}", account.firstName);
+            Console.WriteLine("Last name: {0}", account.lastName);
+            Console.WriteLine("PESEL: {0}", account.pesel);
+            Console.WriteLine();           
+        }
+
+        public void Print(BillingAccount account)
+        {
+            Console.WriteLine("Account owner: {0}", account.GetFullName());
+            Console.WriteLine("Account number: {0}", account.accountNumber);
+            Console.WriteLine("Balance: {0}", account.GetBalance());
+            Console.WriteLine("First name: {0}", account.firstName);
+            Console.WriteLine("Last name: {0}", account.lastName);
+            Console.WriteLine("PESEL: {0}", account.pesel);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Bank_C_Sharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_C_Sharp
+{
+    class Program
+    {
+        static readonly string name = "Name: Bank C#";
+        static readonly string author = "Author: Maciej Szałek";
+        static Printer printer = new Printer();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name);
+            Console.WriteLine(author);
+            Console.WriteLine();
+
+            SavingAccount savingAccount = new SavingAccount("940000000001", 0.0M, "Maciek", "Szalek", "90232093201");
+            SavingAccount savingAccount2 = new SavingAccount("940000000002", 0.0M, "John", "Smith", "85022093000");
+
+            BillingAccount billingAccount = new BillingAccount(savingAccount.accountNumber,
+                savingAccount.balance,
+                savingAccount.firstName,
+                savingAccount.lastName,
+                savingAccount.pesel);
+       
+            printer.Print(savingAccount);
+            printer.Print(savingAccount2);
+            printer.Print(billingAccount);
+
+            Console.ReadKey();
+        }
+    }
+}
